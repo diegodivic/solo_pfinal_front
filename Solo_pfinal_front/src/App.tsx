f90bec7d97159d5591f8d2ae8c5a37194fda143f
Diego Vicentini <diegodivic@gmail.com>
2023-09-09 18:20:40 -0300

    primeiro commit

--- Solo_pfinal_front/src/App.tsx
+++ Solo_pfinal_front/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <> 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Ola lasanha</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
